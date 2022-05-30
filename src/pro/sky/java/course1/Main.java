3f5d2461adfddd500c3369f60ebd1c70848b04d4
Vero0102 <104280568+Vero0102@users.noreply.github.com>
2022-05-30 23:37:18 +0300

    Переделала домашнее задание №5

--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -4,44 +4,69 @@ public class Main {
 
     public static void main(String[] args) {
         // Задача №1.1
-        int[] arr;
-        arr = new int[3];
+        int[] arr = new int[3];
         arr[0] = 1;
         arr[1] = 2;
         arr[2] = 3;
-        System.out.println(arr[0]);
-        System.out.println(arr[1]);
-        System.out.println(arr[2]);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
 
         // Задача №1.2
         float[] b = {1.57f, 7.654f, 9.986f};
-        System.out.println(b[0]);
-        System.out.println(b[1]);
-        System.out.println(b[2]);
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
 
         // Задача №1.3
         boolean[] c = {true, false};
-        System.out.println(c[0]);
-        System.out.println(c[1]);
+        for (int i = 0; i < c.length; i++) {
+            System.out.println(c[i]);
+        }
 
         // Задача №2
-        System.out.println(arr[0] + ", " + arr[1] + ", " + arr[2]);
-        System.out.println(b[0] + ", " + b[1] + ", " + b[2]);
-        System.out.println(c[0] + ", " + c[1]);
+        for (int i = 0; i < arr.length; i++) {
+            if (i != arr.length - 1) {
+                System.out.print(arr[i] + ", ");
+            } else {
+                System.out.println(arr[i]);
+            }
+        }
 
         // Задача №3
-        System.out.println(arr[2] + ", " + arr[1] + ", " + arr[0]);
-        System.out.println(b[2] + ", " + b[1] + ", " + b[0]);
-        System.out.println(c[1] + ", " + c[0]);
-
-        // Задача №4
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(arr[i] + ", ");
+            } else {
+                System.out.println(arr[i]);
+            }
+        }
+        for (int i = b.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(b[i] + ", ");
+            } else {
+                System.out.println(b[i]);
+            }
+        }
+        for (int i = c.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(c[i] + ", ");
+            } else {
+                System.out.println(c[i]);
+            }
+        }
+        //Задача №4
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 != 0) {
                 arr[i] = arr[i] + 1;
             }
+            if (i != arr.length - 1) {
+                System.out.print(arr[i] + ", ");
+            } else {
+                System.out.println(arr[i]);
+            }
         }
-        System.out.println(arr[0] + ", " + arr[1] + ", " + arr[2] + ".");
-
-
     }
 }
+
+
